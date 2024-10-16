1e8b172c5884b9aa689b69efe30493f043635b11
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-16 15:47:27 +0200

    complete snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marcoLanciObj = students.find(student => student.name === 'Marco Lanci' )
+console.log(marcoLanciObj.class);
